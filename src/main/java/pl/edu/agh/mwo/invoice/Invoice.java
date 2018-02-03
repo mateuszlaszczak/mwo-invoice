ec9ae36cb9c22183aa4233bd6f8dd380aaeb6553
student39 <student39@S7N-PROGLANG-9.pcoip.ki.agh.edu.pl>
2018-02-03 15:08:42 +0100

    getsubtotal na getnetvalue

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,22 +1,22 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Map<Product, Integer> products;
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		products.put(product, 0);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		products.put(product, quantity);
 	}
 
-	public BigDecimal getSubtotal() {
+	public BigDecimal getNetValue() {
 		return null;
 	}
 
